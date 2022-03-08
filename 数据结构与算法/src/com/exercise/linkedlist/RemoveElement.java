c29794cb11573e563c6369e37dae1142ae76f0b4
Liu <l2930471234@163.com>
2022-03-08 11:05:22 +0800

    Code to implement data structures and algorithms

---
+++ 数据结构与算法/src/com/exercise/linkedlist/RemoveElement.java
@@ -0,0 +1,31 @@
+package com.exercise.linkedlist;
+
+/**
+ * 移除链表元素
+ */
+
+public class RemoveElement {
+    public static ListNode removeElements(ListNode head, int val) {
+        //删除元素是头节点
+        while (head != null && head.val == val) {
+            head.next = head;
+        }
+        //若头结点为空，提前结束
+        if (head == null) {
+            return head;
+        }
+        // 已确定当前head.val != val
+        ListNode pre = head;
+        ListNode cur = head.next;
+        while (cur != null) {
+            if (cur.val == val) {
+                pre.next = cur.next;
+            } else {
+                pre = cur;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
+}
+
---
+++ 数据结构与算法/src/com/exercise/linkedlist/ReverseList.java
@@ -0,0 +1,21 @@
+package com.exercise.linkedlist;
+
+/**
+ * 反转链表
+ */
+
+public class ReverseList {
+    //双指针法
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode cur = head;
+        ListNode temp = null;
+        while (cur != null) {
+            temp = cur.next;// 保存下一个节点
+            cur.next = prev;
+            prev = cur;
+            cur = temp;
+        }
+        return prev;
+    }
+}
