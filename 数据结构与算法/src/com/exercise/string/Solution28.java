62127abd4e5593c404f5dc5b12fe6a9386efc18b
Liu <l2930471234@163.com>
2022-04-21 13:38:00 +0800

    Code to implement data structures and algorithms

---
+++ 数据结构与算法/src/com/exercise/string/Solution28.java
@@ -0,0 +1,43 @@
+package com.exercise.string;
+
+/**
+ * KMP 算法
+ * leetcode.28 实现 strStr() 函数。
+ * 给你两个字符串 haystack 和 needle ，
+ * 请你在 haystack 字符串中找出 needle 字符串出现的第一个位置（下标从 0 开始）。
+ * 如果不存在，则返回  -1 。
+ */
+
+public class Solution28 {
+    //前缀表（不减一）
+    public int strStr(String haystack, String needle) {
+        if (needle.length() == 0) return 0;
+        int[] next = new int[needle.length()];
+        getNext(next, needle);
+
+        int j = 0;
+        for (int i = 0; i < haystack.length(); i++) {
+            while (j > 0 && needle.charAt(j) != haystack.charAt(i))
+                j = next[j - 1];
+            if (needle.charAt(j) == haystack.charAt(i))
+                j++;
+            if (j == needle.length())
+                return i - needle.length() + 1;
+        }
+        return -1;
+
+    }
+
+    private void getNext(int[] next, String s) {
+        int j = 0;
+        next[0] = 0;
+        for (int i = 1; i < s.length(); i++) {
+            while (j > 0 && s.charAt(j) != s.charAt(i))
+                j = next[j - 1];
+            if (s.charAt(j) == s.charAt(i))
+                j++;
+            next[i] = j;
+        }
+    }
+}
+
